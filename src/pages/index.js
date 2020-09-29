cd308530cc837de78153cd6bb5c79f2edbfaf012
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-09-29 17:50:21 +0100

    Added Hero and style.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import React from "react"
 import Layout from "../components/layout";
+import styled from 'styled-components';
+import Hero from "../components/sections/hero";
+
+const StyledMainContainer = styled.main`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin: 0 0 0 11rem;
+`;
+
 
 const Index = () => {
   return (
     <Layout>
-      <h1 className="big-heading">Sam Brocklehurst</h1>
+      <StyledMainContainer>
+        <Hero />
+        <Projects />
+      </StyledMainContainer>
     </Layout>
   )
 }
