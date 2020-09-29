9522b61997813de2e49bc68f613bc89cf9b6e803
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-09-30 00:09:40 +0100

    Added metadata and plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,31 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Sam Brocklehurst",
+    description:
+      "Sam Brocklehurst is currently studying Web Technologies at the Univeristy of Huddersifeld, final year.",
+    siteUrl: "https://sambrock.net",
+    image: "/og.png",
+    twitterUsername: "@sxmbrock",
+  },
   plugins: [
-    
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/json/`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`
   ],
 }
