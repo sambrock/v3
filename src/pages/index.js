6a1658376e7e339456efdf94b1f6428a31a793d0
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-09-30 00:09:15 +0100

    Added index container.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,51 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout";
 import styled from 'styled-components';
 import Hero from "../components/sections/hero";
+import Projects from "../components/projects";
 
 const StyledMainContainer = styled.main`
   display: grid;
   grid-template-columns: 1fr 1fr;
   margin: 0 0 0 11rem;
+
+  @media(max-width: 768px) {
+    grid-template-columns: 1fr;
+    margin: 0 2rem;
+  }
 `;
 
+const Index = ({ data }) => {
+  const [projects, setProjects] = useState([]);
 
-const Index = () => {
+  useEffect(() => {
+    setProjects(data.projects.edges);
+  }, [data])
+
   return (
     <Layout>
       <StyledMainContainer>
         <Hero />
-        <Projects />
+        <Projects projects={projects} />
       </StyledMainContainer>
     </Layout>
   )
 }
+
+export const query = graphql`
+query ProjectsQuery {
+  projects: allProjectsJson {
+    edges {
+      node {
+        id
+        title
+        type
+        shortDescription
+        color
+      }
+    }
+  }
+}
+`;
 
 export default Index;
