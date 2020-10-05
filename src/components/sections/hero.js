04e7d3028217c9b488f6efa9a894fd575d98a52c
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-10-06 00:20:23 +0100

    Cleanup. Removed dynamic style.

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -6,13 +6,15 @@ const StyledHeroSection = styled.section`
   flex-direction: column;
   height: 100vh;
   justify-content: center;
+  padding: 0 
+  7rem;
 
   h1 {
     margin: 0 0 .5rem 0;
   }
 
   h2 {
-    margin: 0 0 2rem 0;
+    margin: 0 0 2.5rem 0;
   }
 
   .email-link {
@@ -24,9 +26,9 @@ const StyledHeroSection = styled.section`
 const Hero = () => {
   return (
     <StyledHeroSection>
-      <h1 className="heading">Sam Brocklehurst.</h1>
+      <h1 className="heading">Sam Brocklehurst</h1>
       <h2 className="sub-heading">I design and develop web apps.</h2>
-      <button class="email-link" style={{color: '#F27405', border: '1px solid #F27405'}}>Get in touch</button>
+      <button class="email-link">Get in touch</button>
     </StyledHeroSection>
   )
 }
