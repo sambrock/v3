430d85105b5ebd54f46c094b9cf7ea1564a85cc9
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-10-06 00:20:03 +0100

    Added draggable slider for projects.

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,87 +1,88 @@
-import React, { useState } from 'react';
-import { graphql } from 'gatsby';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
-import ScrollWheelHandler from 'react-scroll-wheel-handler';
-import IconLogo from './icons/logo';
+import { animated,  useSpring } from 'react-spring';
+
 import ProjectSlide from './project-slide';
+import LogoTransparent from '../images/logo-w.svg';
 
-const StyledProjectsContainer = styled.section`
+const StyledProjectsContainer = styled(animated.section)`
+  height: calc(100vh);
   position: relative;
-  margin: 3rem 0;
+  /* margin: 0 3rem 0 0; */
   overflow: hidden;
-`;
+  background:var(--off-white) url(${LogoTransparent}) no-repeat center;
+  background-size: 100% calc(100vh - 6rem);
+  padding: 0 3rem;
+  mix-blend-mode: multiply;
 
-const StyledBackgroundLogo = styled.div`
-  height: calc(100vh - 6rem);
-  width: 100%;
-  overflow: hidden;
-  position: absolute;
-  top: 0;
-  z-index: 0;
-
-  svg {
-    height: 100%;
-    fill: rgba(255,255,255, .07);
+  .projects__slider {
+    overflow: auto;
+    overflow-x: hidden;
+    cursor: grab;
+    touch-action: pan-x;
+    margin: 0 4rem;
   }
-
 `;
 
 const Projects = ({ projects }) => {
-  const [active, setActive] = useState(0);
-  const [animate, setAnimate] = useState(null);
-  const [isAnimating, setIsAnimating] = useState(null);
-  const [disableAnimation, setDisableSlideAniamtion] = useState(false);
+  const [sliderPosY, setSliderPosY] = useState(0);
+  const [mouseDown, setMouseDown] = useState(false);
 
-  const handleNavigation = (scroll) => {
-    // if (isMobile || disableAnimation) return;
-    setIsAnimating(true);
+  let sliderRef = useRef();
+  let mouseDownPos;
+  let sliderPos;
 
-    let next = active;
+  const getSliderPositions = () => {
+    const increment = sliderRef.current.offsetHeight / projects.length;
+    let arr = [];
+    let i;
 
-    if (scroll === 'up') next--;
-    if (scroll === 'down') next++;
+    for (i = 0; i < projects.length; i++) {
+      arr.push(-Math.abs(i * increment))
+    }
 
-    if (next === projects.length) next = 0;
-    if (next === -1) next = (projects.length - 1);
+    return arr;
+  }
 
-    setAnimate({ index: active, type: 'out', direction: scroll });
+  const handleMouseDown = (e) => {
+    mouseDownPos = e.clientY;
+    sliderRef.current.style.cursor = 'grabbing';
 
-    setTimeout(() => {
-      setAnimate({ index: next, type: 'in', direction: scroll });
-    }, 650)
+    setMouseDown(true);
 
-    // Delay to prevent glitchy animation (due to GSAP fromTo)
-    setTimeout(() => {
-      setActive(next);
-    }, 690)
-
-    setTimeout(() => {
-      setIsAnimating(false);
-    }, 1200)
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
   };
+
+
+  const handleMouseMove = (e) => {
+    const dy = e.clientY - mouseDownPos;
+    sliderPos = sliderPosY + (dy * 4);
+    setSliderPosY(sliderPos);
+  };
+
+  const handleMouseUp = () => {
+    const closest = getSliderPositions().sort((a, b) => Math.abs(sliderPos - a) - Math.abs(sliderPos - b))[0];
+    setSliderPosY(closest);
+
+    setMouseDown(false);
+
+    sliderRef.current.style.cursor = 'grab';
+
+    document.removeEventListener('mousemove', handleMouseMove);
+    document.removeEventListener('mouseup', handleMouseUp);
+  };
+
+  const props = useSpring(mouseDown ? { transform: `translate(0px, ${sliderPosY}px)`, config: { mass: 1, tension: 40 } } : { transform: `translate(0px, ${sliderPosY}px)`, config: { mass: 1, tension: 200 } });
 
   return (
     <StyledProjectsContainer>
-      <ScrollWheelHandler upHandler={() => handleNavigation('up')} downHandler={() => handleNavigation('down')}>
-        {projects.map((projectNode, index) => {
-          const project = projectNode.node;
-          return (
-            <ProjectSlide key={project.id}
-              project={project}
-              isActive={index === active}
-              animate={animate}
-              current={active + 1}
-              indicator={index === 0}
-              isAnimating={isAnimating}
-              disableSlideAnimation={setDisableSlideAniamtion}
-            />
-          )
-        })}
-        <StyledBackgroundLogo>
-          <IconLogo />
-        </StyledBackgroundLogo>
-      </ScrollWheelHandler>
-    </StyledProjectsContainer>
+      <animated.div className="projects__slider" style={props} ref={sliderRef} onMouseDown={(e) => handleMouseDown(e)}>
+        {projects.map((project, i) => (
+          <ProjectSlide key={project.id} project={project} />
+        ))}
+      </animated.div>
+    </StyledProjectsContainer >
   )
 }
 
