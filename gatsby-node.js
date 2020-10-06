7d0b819428c10d333e84cd74302a6fab6666cef3
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-10-06 17:12:39 +0100

    Added create pages API.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,26 @@
-// const path = require(`path`);
+const path = require(`path`);
 
-// exports.createPages = async function ({ actions, graphql }) {
-//   const { data } = await graphql(`
-//     query {
-//       projects: allProjectsJson {
-//         edges {
-//           node {
-//             id
-//             title
-//           }
-//         }
-//       }
-//     }
-//   `)
-//   data.projects.edges.forEach(edge => {
-//     const id = edge.node.id;
-//     const title = edge.node.title;
-//     console.log(title);
-//     actions.createPage({
-//       path: title.toLowerCase(),
-//       component: require.resolve(`./src/templates/project.js`),
-//       context: { id: id },
-//     })
-//   })
-// }
+exports.createPages = async function ({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      projects: allProjectsJson {
+        edges {
+          node {
+            id
+            title
+          }
+        }
+      }
+    }
+  `)
+  data.projects.edges.forEach(edge => {
+    const id = edge.node.id;
+    const title = edge.node.title;
+    console.log(title);
+    actions.createPage({
+      path: title.toLowerCase(),
+      component: require.resolve(`./src/templates/project.js`),
+      context: { id: id },
+    })
+  })
+}
