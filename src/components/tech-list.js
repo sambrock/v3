896ade606260a6d37be12935a0cc3e7ed13debe6
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-10-09 20:22:01 +0100

    Cleanup.

--- src/components/tech-list.js
+++ src/components/tech-list.js
@@ -1,66 +1,59 @@
 import React from 'react'
 import ReactTooltip from 'react-tooltip';
 import styled from 'styled-components';
-
-import { IconReact, IconNode, IconMongoDB, IconGulp, IconIllustrator, IconPhotoshop } from './icons'
+import { StyledIconBase } from '@styled-icons/styled-icon'
+import { ReactLogo, Nodejs } from '@styled-icons/boxicons-logos';
+import { Mongodb, Adobeillustrator, Adobephotoshop, Gulp } from '@styled-icons/simple-icons';
 
 const StyledTechList = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
-    .tech {
-      margin: 0 .75rem 0 0;
+  .tech {
+    margin: 0 .75rem 0 0;
 
-      &.no-icon {
-        margin: 0 .75rem 5px 0;
-        font-size: .9em;
-        font-family: $medium;
-      }
+    &.no-icon {
+      margin: 0 .75rem 5px 0;
+      font-size: .9em;
     }
+  }
 
-    .tech-icon-tooltip {
-      background: var(--black) !important;
-      color: $white !important;
-      border-radius: 0px !important;
-      font-weight: 500;
+  .tech-icon-tooltip {
+    background: var(--black) !important;
+    border-radius: 0px !important;
+    font-weight: 600;
 
     .place-bottom::after {
       border-bottom-color: var(--white) !important;
     }
   }
 
-  .tech-icon {
-    height: 1.75em;
-    fill: var(--black);
+  ${StyledIconBase} {
+    color: var(--black);
+    width: 2em;
   }
 `;
 
 const TechList = ({ items }) => {
   return (
     <StyledTechList>
-      <ReactTooltip className="tech-icon-tooltip" />
       {items.includes('react') && (
-        <a href="https://reactjs.org/" target="__blank" data-tip="React.js" data-place="bottom" className="tech" aria-label="React.js"><IconReact classes="tech-icon" /></a>
+        <a href="https://reactjs.org/" target="__blank" data-tip="React.js" data-place="bottom" className="tech" aria-label="React.js"><ReactLogo /></a>
       )}
-      <ReactTooltip className="tech-icon-tooltip" />
       {items.includes('node') && (
-        <a href="https://nodejs.org/" target="__blank" data-tip="Node.js" data-place="bottom" className="tech" aria-label="Node.js"><IconNode classes="tech-icon" /></a>
+        <a href="https://nodejs.org/" target="__blank" data-tip="Node.js" data-place="bottom" className="tech" aria-label="Node.js"><Nodejs /></a>
       )}
-      <ReactTooltip className="tech-icon-tooltip" />
       {items.includes('mongodb') && (
-        <a href="https://www.mongodb.com/" target="__blank" data-tip="MongoDB" data-place="bottom" className="tech" aria-label="MongoDB"><IconMongoDB classes="tech-icon" /></a>
+        <a href="https://www.mongodb.com/" target="__blank" data-tip="MongoDB" data-place="bottom" className="tech" aria-label="MongoDB"><Mongodb /></a>
       )}
-      <ReactTooltip className="tech-icon-tooltip" />
       {items.includes('gulp') && (
-        <a href="https://gulpjs.com/" target="__blank" data-tip="gulp.js" data-place="bottom" className="tech" aria-label="Gulp.js"><IconGulp classes="tech-icon" /></a>
+        <a href="https://gulpjs.com/" target="__blank" data-tip="gulp.js" data-place="bottom" className="tech" aria-label="Gulp.js"><Gulp /></a>
       )}
-      <ReactTooltip className="tech-icon-tooltip" />
       {items.includes('illustrator') && (
-        <a href="https://www.adobe.com/uk/products/illustrator.html" target="__blank" data-tip="Adobe Illustrator" data-place="bottom" className="tech" aria-label="Illustrator"><IconIllustrator classes="tech-icon" /></a>
+        <a href="https://www.adobe.com/uk/products/illustrator.html" target="__blank" data-tip="Adobe Illustrator" data-place="bottom" className="tech" aria-label="Illustrator"><Adobeillustrator /></a>
       )}
-      <ReactTooltip className="tech-icon-tooltip" />
       {items.includes('photoshop') && (
-        <a href="https://www.adobe.com/uk/products/photoshop.html" target="__blank" data-tip="Adobe Photoshop" data-place="bottom" className="tech" aria-label="Photoshop"><IconPhotoshop classes="tech-icon" /></a>
+        <a href="https://www.adobe.com/uk/products/photoshop.html" target="__blank" data-tip="Adobe Photoshop" data-place="bottom" className="tech" aria-label="Photoshop"><Adobephotoshop /></a>
       )}
       {items.includes('express') && (
         <a href="https://expressjs.com/" target="__blank" className="tech no-icon" aria-label="Express">Express</a>
@@ -71,6 +64,7 @@ const TechList = ({ items }) => {
       {items.includes('protopie') && (
         <a href="https://www.protopie.io/" target="__blank" className="tech no-icon" aria-label="ProtoPie">ProtoPie</a>
       )}
+      <ReactTooltip className="tech-icon-tooltip" />
     </StyledTechList>
   )
 }
