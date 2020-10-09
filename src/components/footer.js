575a97d98ab50e3df7237ac47ded6d6c1f861a40
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-10-09 20:21:38 +0100

    HIde footer on page transition.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import VizSensor from 'react-visibility-sensor';
 
 import SocialIcons, { Email } from './social-icons';
+import { TransitionContext } from './transition-context';
 
 const StyledFooter = styled.footer`
   width: 100vw;
@@ -10,32 +11,49 @@ const StyledFooter = styled.footer`
   align-items: baseline;
   z-index: 11;
   bottom: 0;
-  padding: 0 25px;
+  padding: 3rem 25px;
+  position: relative;
+
+  span {
+    display: block;
+  }
 
   .copyright {
     font-weight: 500;
     color: var(--black-light);
     font-size: 14px;
     text-align: center;
-    margin: 3rem 0;
+  }
 
-    @media(max-width: 480px) {
-      margin: 50px 0 25px;
+  @media(max-width: 768px) {
+    z-index: 3;
+  }
+  
+  @media(max-width: 480px) {
+    padding: 0 0 25px 0;
+
+    .copyright {
+      padding: 25px 0 0 0;
     }
   }
 `;
 
 const Footer = () => {
+  const { transition } = useContext(TransitionContext);
   const [reveal, setReveal] = useState(false);
+
+  useEffect(() => {
+    setReveal(false);
+  }, [transition])
 
   return (
     <StyledFooter>
-      <SocialIcons isMobile={true} />
-      <Email isMobile={true} />
+      <SocialIcons />
+      <Email />
       <VizSensor onChange={(isVisible) => setReveal(isVisible)} active={!reveal}>
-        <span className={`copyright ${reveal ?  'fadeup-enter-active' : 'fadeup-enter'}`}>Designed &amp; built by Sam Brocklehurst</span>
+        <span className={`copyright ${reveal ? 'fadeup-enter-active' : 'fadeup-enter'} ${transition.play ? 'hide' : ''}`}>Designed &amp; built by Sam Brocklehurst</span>
       </VizSensor>
-    </StyledFooter>
+    </StyledFooter >
   )
 }
 
