168e0e90cd7c305201f45037c833e583aff5dd43
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-01-20 21:20:34 +0000

    Added new tech.

--- src/components/sections/project-info.js
+++ src/components/sections/project-info.js
@@ -5,9 +5,9 @@ import TechList from '../tech-list';
 const ProjectInfo = ({ project }) => {
   return (
     <section className="flex py-20 px-8 sm:px-24 flex-col sm:flex-row justify-between">
-      <div className="flex flex-col w-full sm:w-2/3">
+      <div className="flex flex-col w-full sm:w-2/3 mr-12">
         <h2 className="text-xxl sm:text-heading">{project.shortDescription}</h2>
-        <p className="text-base my-8">{project.description}</p>
+        <p className="text-base my-8" dangerouslySetInnerHTML={{ __html: project.description }}></p>
         <div className="mt-0">
           <a className="btn" target="__blank" rel="noopener" style={{ color: project.color, border: `1px ${project.color} solid` }} href={project.url}>Launch project</a>
           {project.github && (
@@ -19,12 +19,12 @@ const ProjectInfo = ({ project }) => {
         <div className="flex">
           <div className="mr-auto sm:mr-12">
             <h3 style={{ color: project.color }}>Completed</h3>
-            <span className="">{project.completed}</span>
+            <span>{project.completed}</span>
           </div>
           <div>
             <h3 style={{ color: project.color }}>My role</h3>
             <ul>
-              {project.role.map((r, i) => <li className="" key={i}>{r}</li>)}
+              {project.role.map((r, i) => <li key={i}>{r}</li>)}
             </ul>
           </div>
         </div>
--- src/components/tech-list.js
+++ src/components/tech-list.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import { StyledIconBase } from '@styled-icons/styled-icon'
 import { ReactLogo, Nodejs } from '@styled-icons/boxicons-logos';
-import { Mongodb, Adobeillustrator, Adobephotoshop, Gulp } from '@styled-icons/simple-icons';
+import { Mongodb, Adobeillustrator, Adobephotoshop, Gulp, Redux, Express } from '@styled-icons/simple-icons';
 import ReactTooltip from 'react-tooltip';
 
 const StyledTechList = styled.div`
@@ -38,31 +38,34 @@ const TechList = ({ items }) => {
   return (
     <StyledTechList>
       {items.includes('react') && (
-        <a href="https://reactjs.org/" target="__blank" data-tip="React.js" data-place="bottom" className="tech" aria-label="React.js"><ReactLogo /></a>
+        <a href="https://reactjs.org/" rel="noopener noreferrer" target="__blank" data-tip="React.js" data-place="bottom" className="tech" aria-label="React.js"><ReactLogo /></a>
       )}
       {items.includes('node') && (
-        <a href="https://nodejs.org/" target="__blank" data-tip="Node.js" data-place="bottom" className="tech" aria-label="Node.js"><Nodejs /></a>
+        <a href="https://nodejs.org/" rel="noopener noreferrer" target="__blank" data-tip="Node.js" data-place="bottom" className="tech" aria-label="Node.js"><Nodejs /></a>
       )}
       {items.includes('mongodb') && (
-        <a href="https://www.mongodb.com/" target="__blank" data-tip="MongoDB" data-place="bottom" className="tech" aria-label="MongoDB"><Mongodb /></a>
+        <a href="https://www.mongodb.com/" rel="noopener noreferrer" target="__blank" data-tip="MongoDB" data-place="bottom" className="tech" aria-label="MongoDB"><Mongodb /></a>
+      )}
+      {items.includes('redux') && (
+        <a href="https://redux.js.org/" rel="noopener noreferrer" target="__blank" data-tip="Redux" data-place="bottom" className="tech" aria-label="Redux"><Redux /></a>
       )}
       {items.includes('gulp') && (
-        <a href="https://gulpjs.com/" target="__blank" data-tip="gulp.js" data-place="bottom" className="tech" aria-label="Gulp.js"><Gulp /></a>
+        <a href="https://gulpjs.com/" rel="noopener noreferrer" target="__blank" data-tip="gulp.js" data-place="bottom" className="tech" aria-label="Gulp.js"><Gulp /></a>
       )}
       {items.includes('illustrator') && (
-        <a href="https://www.adobe.com/uk/products/illustrator.html" target="__blank" data-tip="Adobe Illustrator" data-place="bottom" className="tech" aria-label="Illustrator"><Adobeillustrator /></a>
+        <a href="https://www.adobe.com/uk/products/illustrator.html" rel="noopener noreferrer" target="__blank" data-tip="Adobe Illustrator" data-place="bottom" className="tech" aria-label="Illustrator"><Adobeillustrator /></a>
       )}
       {items.includes('photoshop') && (
-        <a href="https://www.adobe.com/uk/products/photoshop.html" target="__blank" data-tip="Adobe Photoshop" data-place="bottom" className="tech" aria-label="Photoshop"><Adobephotoshop /></a>
+        <a href="https://www.adobe.com/uk/products/photoshop.html" rel="noopener noreferrer" rel="noopener noreferrer" target="__blank" data-tip="Adobe Photoshop" data-place="bottom" className="tech" aria-label="Photoshop"><Adobephotoshop /></a>
       )}
       {items.includes('express') && (
-        <a href="https://expressjs.com/" target="__blank" className="tech no-icon" aria-label="Express whitespace-nowrap">Express</a>
+        <a href="https://expressjs.com/" rel="noopener noreferrer" target="__blank" className="tech no-icon" aria-label="Express whitespace-nowrap">Express</a>
       )}
       {items.includes('tmdb api') && (
-        <a href="https://developers.themoviedb.org/3/" target="__blank" className="tech no-icon whitespace-nowrap" aria-label="TMDb API">TMDb API</a>
+        <a href="https://developers.themoviedb.org/3/" rel="noopener noreferrer" target="__blank" className="tech no-icon whitespace-nowrap" aria-label="TMDb API">TMDb API</a>
       )}
       {items.includes('protopie') && (
-        <a href="https://www.protopie.io/" target="__blank" className="tech no-icon whitespace-nowrap" aria-label="ProtoPie">ProtoPie</a>
+        <a href="https://www.protopie.io/" rel="noopener noreferrer" target="__blank" className="tech no-icon whitespace-nowrap" aria-label="ProtoPie">ProtoPie</a>
       )}
       <ReactTooltip className="tech-icon-tooltip" />
     </StyledTechList>
