5eb7fd965d508ff4ea21d0c3f00c5c9414f8d22f
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-01-21 02:25:57 +0000

    Updated responsive styles.

--- src/components/sections/project-info.js
+++ src/components/sections/project-info.js
@@ -4,9 +4,9 @@ import TechList from '../tech-list';
 
 const ProjectInfo = ({ project }) => {
   return (
-    <section className="flex py-20 px-8 sm:px-24 flex-col sm:flex-row justify-between">
-      <div className="flex flex-col w-full sm:w-2/3 mr-12">
-        <h2 className="text-xxl sm:text-heading">{project.shortDescription}</h2>
+    <section className="flex py-20 px-8 md:px-24 flex-col lg:flex-row justify-between">
+      <div className="flex flex-col w-full lg:w-2/3 mr-12">
+        <h2 className="text-heading">{project.shortDescription}</h2>
         <p className="text-base my-8" dangerouslySetInnerHTML={{ __html: project.description }}></p>
         <div className="mt-0">
           <a className="btn" target="__blank" rel="noopener" style={{ color: project.color, border: `1px ${project.color} solid` }} href={project.url}>Launch project</a>
@@ -15,7 +15,7 @@ const ProjectInfo = ({ project }) => {
           )}
         </div>
       </div>
-      <div className="flex flex-col mt-20 sm:mt-0">
+      <div className="flex flex-col mt-20 lg:mt-0">
         <div className="flex">
           <div className="mr-auto sm:mr-12">
             <h3 style={{ color: project.color }}>Completed</h3>
